b1a34c0e1a8cf3d6d4aab2263d2133da07b1478b
Sergey Mishin <sergei.a.mishin@gmail.com>
2024-12-07 23:32:03 +0100

    Update 2024 flake

--- 2024/flake.nix
+++ 2024/flake.nix
@@ -1,24 +1,30 @@
 {
-  description = "Roc flake template";
+  description = "Advent of Code 2024";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
     roc.url = "github:roc-lang/roc";
   };
 
-  outputs = { self, nixpkgs, flake-utils, roc, ... }:
+  outputs = { self, nixpkgs, flake-utils, roc, nixpkgs-mozilla, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import nixpkgs-mozilla) ];
+        };
 
-        # see "packages =" in https://github.com/roc-lang/roc/blob/main/flake.nix
         rocPkgs = roc.packages.${system};
-
         rocFull = rocPkgs.full;
 
         haskell = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [ cabal-install ]);
 
+        rusttoolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          sha256 = "sha256-VZZnlyP69+Y3crrLHQyJirqlHrTtGTsyiSnZB8jEvVo=";
+        }).rust;
       in
       {
         formatter = pkgs.nixpkgs-fmt;
@@ -30,9 +36,11 @@
                 rocFull # includes CLI
                 haskell
                 haskell-language-server
+                rusttoolchain
               ];
 
-            # For vscode plugin https://github.com/ivan-demchenko/roc-vscode-unofficial
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+
             shellHook = ''
               export ROC_LANGUAGE_SERVER_PATH=${rocFull}/bin/roc_language_server
             '';
