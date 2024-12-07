e8134982ecc09b3f1d157e20f93bbc6dddb4de00
Sergey Mishin <sergei.a.mishin@gmail.com>
2024-12-07 23:06:35 +0100

    2024-02: add part2 in haskell

--- 2024/day02/haskell/app/Main.hs
+++ 2024/day02/haskell/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Environment (getArgs)
+import Data.Sequence (mapWithIndex, foldlWithIndex, fromList)
 
 ruleAllAre :: (a -> a -> Bool) -> [a] -> Bool
 ruleAllAre _ [] = True
@@ -26,8 +27,18 @@ ruleDiffInRangeIncl from to lst =
     ruleAllAre diffInRange lst
 
 
-reportIsSafe :: [Int] -> Bool
+reportIsSafe :: Ord a => Num a => [a] -> Bool
 reportIsSafe lst = (ruleAllInc lst || ruleAllDec lst) && ruleDiffInRangeIncl 1 3 lst
+
+splitAtWithout :: Int -> [a] -> [a]
+splitAtWithout n lst = take n lst ++ drop (n+1) lst
+
+everyWithout1 :: [a] -> [[a]]
+-- everyWithout1 lst = mapWithIndex (\i _ -> splitAtWithout i lst) lst
+everyWithout1 lst = foldlWithIndex (\acc i _ -> acc ++ [splitAtWithout i lst]) [] (fromList lst)
+
+reportIsSafeDamp :: [Int] -> Bool
+reportIsSafeDamp = any reportIsSafe . everyWithout1
 
 lineIntoInts :: String -> [Int]
 lineIntoInts = map read . words
@@ -57,3 +68,12 @@ main = do
   let r = filter id safe
   let total = length r
   putStrLn $ "part1: " ++ show total
+
+  -- print $ (reportIsSafe . everyWithout1) [1, 2, 3, 4]
+  -- let listOfLists = everyWithout1 [1, 10, 0, 10, 0]
+  -- let res = any reportIsSafe listOfLists
+  -- print res
+
+  let safe2 = length $ filter id $ map reportIsSafeDamp nums
+
+  putStrLn $ "part2: " ++ show safe2
