bf24d486ad2e6e7164f9f77c4f9dcdbf20111765
Sergey Mishin <sergei.a.mishin@gmail.com>
2025-12-01 17:50:48 +0100

    Init 2025

---
+++ 2025/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Advent of Code 2025";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-mozilla, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import nixpkgs-mozilla) ];
+        };
+
+        rusttoolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          sha256 = "sha256-SDu4snEWjuZU475PERvu+iO50Mi39KVjqCeJeNvpguU=";
+        }).rust;
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs;
+              [
+                rusttoolchain
+                zig
+              ];
+
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+        };
+      });
+}
